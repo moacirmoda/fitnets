c0f362d4ec4a1151918cde56a7afb9273f2b48c5
moacirmoda <moa.moda@gmail.com>
2012-08-29 17:01:40 -0300

    adding some details

--- html/static/js/functions.js
+++ html/static/js/functions.js
@@ -1,7 +1,13 @@
 $(function(){
 
-    // toggle dos blocks
+    var body = $('body');
     var block = $('.block');    
+
+    // efeito em todos itens block para aparecem ao dar load
+    body.hide();
+    body.fadeIn(1000);
+
+    // toggle dos blocks
     block.find('h3').click(function(){
         $(this).next('.blockContent').slideToggle('fast');
     });
