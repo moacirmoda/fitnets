a7a3e6c7af99d2776002021e57ae72259a12b310
moacirmoda <moa.moda@gmail.com>
2012-09-26 19:04:45 -0300

    atualizando o readme

--- atualiza-svn.sh
+++ atualiza-svn.sh
@@ -2,8 +2,11 @@ svn co https://fw.lab.sp.cefetsp.br/svn/a6pgp/S201202/Fitnets/ svn
 cd svn
 git clone git://github.com/moacirmoda/fitnets.git git
 cp -r git/* .
-rm -r git
+rm -rf git
+ls | xargs svn add
+
 svn ci -m "Atualizando svn"
+
 cd ..
-rm -r svn
+rm -rf svn
 
