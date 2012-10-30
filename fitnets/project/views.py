cbee8eaeff94a3b3111842e3e1c96edac5a735f4
moacirmoda <moa.moda@gmail.com>
2012-10-30 16:01:24 -0200

    ammend

--- fitnets/project/views.py
+++ fitnets/project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.http import HttpResponse
 from models import *
@@ -22,3 +23,24 @@ def new(request):
     output['form'] = form
 
     return render_to_response("project/new.html", output, context_instance=RequestContext(request))
+
+def list(request, username, type=None):
+
+    output = {}
+    user = User.objects.get(username=username)
+    finished = False
+    if type == 'closed':
+        finished = True
+
+    if type == "all":
+        projects = Project.objects.filter(creator=user)[:10]
+    else:
+        projects = Project.objects.filter(creator=user).filter(finished=finished)[:10]
+
+    output['user'] = user
+    output['projects'] = projects
+    output['finished'] = finished
+    output['type'] = type
+
+    return render_to_response("project/list.html", output, context_instance=RequestContext(request))
+
