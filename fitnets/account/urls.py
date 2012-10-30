ae770d1dc67c7541164fa49a2276a0c9d8568cac
moacirmoda <moa.moda@gmail.com>
2012-10-30 14:46:53 -0200

    #50: alterando url de profile do usuário

--- fitnets/account/urls.py
+++ fitnets/account/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^dashboard/?$', 'account.views.dashboard'),
     url(r'^edit_profile/?$', 'account.views.edit_profile'),
-    url(r'^(?P<username>[\w_-]+)/?$', 'account.views.profile'),
+    url(r'^user/(?P<username>[\w_-]+)/?$', 'account.views.profile'),
 )
